53c8a6351f58cc945e939ca0d188e3baadd0b615
Franklyne Kipngetich <franklyne22.chebet@gmail.com>
2026-05-12 19:37:24 +0300

    Initial commit - TenantCoreSystem

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Digital Tenant System</title>
+</head>
+<body>
+
+<h1>Welcome to Digital Tenant Management System</h1>
+<a href="login.php">Go to Login</a>
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ . '/includes/db.php';
+echo "Database Connected Successfully";
+?>
+
